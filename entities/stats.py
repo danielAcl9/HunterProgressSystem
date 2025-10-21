ad44d0ab4127ebb5de095984d8ea339c594988f7
Daniel ICT <dafeamca99@gmail.com>
2025-10-21 14:16:30 -0500

    Created function XP for next level

--- entities/stats.py
+++ entities/stats.py
@@ -8,25 +8,29 @@ class Stats():
     def __init__(self, name: str, total_xp: int) -> None:
         self.name = name
         self.total_xp = total_xp
-        # Revisar
-        # level = Stats.get_level(self.total_xp)
 
-    def get_level(self, total_xp: int) -> int:
+    def get_level(self) -> int:
         level = 1
         # Recorre el largo del threshold
         for i in range(len(XP_THRESHOLDS)):
-            if total_xp >= XP_THRESHOLDS[i]:
+            if self.total_xp >= XP_THRESHOLDS[i]:
                 # Devuelve el primer nivel al que sa mayor.
                 level = i + 1
             else:
                 break
         return level
             
+    def xp_for_next_level(self) -> str:
+        next_lvl = self.get_level()
+        if next_lvl > len(XP_THRESHOLDS):
+            return 0
+        else:
+            xp_next_lvl = XP_THRESHOLDS[next_lvl]
+            xp_needed = xp_next_lvl - self.total_xp
+            return f'XP necesaria para el siguiente nivel: {xp_needed} XP'
 
 # TODO: Resolver de acá para abajo
             
-    # def xp_for_next_level(self):
-    #     current_lvl = Stats.get_level(self.total_xp)
 
     # def add_exp(self, exp: int) -> str:
     #     self.total_xp += exp
@@ -34,8 +38,6 @@ class Stats():
     #     return f'Se añadió {exp} puntos de experiencia. Experiencia actual de la clase {self.name}: {self.total_xp} XP'
 
 
-stat = Stats("Fuerza", 0)
-print(f'Nivel actual: {stat.get_level(50)}')
-print(f'Nivel actual: {stat.get_level(100)}')
-print(f'Nivel actual: {stat.get_level(300)}')
-print(f'Nivel actual: {stat.get_level(1000)}')
+stat = Stats("Fuerza", 250)
+# print(f'Nivel actual: {stat.get_level(50)}')
+print(stat.xp_for_next_level())
