6fb64608cb3b74faa653bf710e4b8500bac9609c
Daniel ICT <dafeamca99@gmail.com>
2025-10-24 15:40:27 -0500

    wip(repositories): start HunterRepository skeleton (incomplete)

---
+++ repositories/hunter_repository.py
@@ -0,0 +1,28 @@
+import json
+import os
+from typing import Optional
+from entities.hunter import Hunter
+from entities.stats import Stat
+
+class HunterRepository:
+
+    def __init__(self, root: str) -> None:
+        # Recibe la ruta del archivo JSON donde guardará datos
+        self.root = root
+    
+    def _data_exists(self) -> None:
+        # Método privado: Asegurar que existe la carpeta data
+        pass
+
+    def _hunter_to_dict(self, hunter: Hunter) -> None:
+        self.hunter = Hunter
+        dict_hunter = dict([
+            ('Name', self.hunter.name),
+            ('Gold', self.hunter.gold)
+            # Ahora, como guardo las clases? 
+        ])
+        # Convertir un objeto Hunter en un diccionario para guardar en JSON
+        pass
+
+
+    # TODO
