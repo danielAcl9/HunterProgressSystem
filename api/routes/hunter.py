4bd7a0e4d2a4a70c166d00c0bd0c3d88b6a98dc0
Daniel Amado <dafeamca99@gmail.com>
2025-11-15 20:12:01 -0500

    feat(api): add hunter profile endpoints

---
+++ api/routes/hunter.py
@@ -0,0 +1,52 @@
+from fastapi import APIRouter, HTTPException
+from repositories.hunter_repository import HunterRepository
+
+router = APIRouter(prefix="/hunter", tags=["Hunter"])
+
+# Initialize Repository
+hunter_repo = HunterRepository("data/hunter.json")
+
+@router.get("/profile")
+def get_gunter_profile():
+    hunter = hunter_repo.load()
+
+    # Convert stats into a serializable format
+    stats_data = {}
+    for stat_name, stat in hunter.stats.items():
+        stats_data[stat_name] = {
+            "name": stat.name,
+            "level": stat.get_level(),
+            "total_xp": stat.total_xp,
+            "xp_for_next_level": stat.xp_for_next_level()
+        }
+
+    return {
+        "name": hunter.name,
+        "global level": hunter.get_global_level(),
+        "total_XP": stat.total_xp,
+        "gold": hunter.gold,
+        "stats": stats_data
+    }
+
+@router.put("/profile")
+def update_hutner_profile(name: str = None, gold: int = None):
+    hunter = hunter_repo.load()
+
+    # Update fields if included
+    if name is not None:
+        if not name.strip():
+            raise HTTPException(status_code=400, detail="Name cannot be empty.")
+        hunter.name = name
+    
+    if gold is not None:
+        if gold < 0:
+            raise HTTPException(status_code=400, detail="Gold cannot be negative")
+        hunter.gold = gold
+
+    hunter_repo.save(hunter)
+
+    return {
+        "message": "Hunter updated succesfully",
+        "name": hunter.name,
+        "gold": hunter.gold
+    }
